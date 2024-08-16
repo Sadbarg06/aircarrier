8025976fb4adeb9c8b65d00a0e91fe6062f836b8
Sadbarg <sadbargo4ka93@yandex.ru>
2024-08-16 19:56:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int ticket = 12_000;
+        int bonus = 20;
+        int bonusMile = ticket / bonus;
+
+        System.out.println("При покупке билета стоимостью " + ticket + " руб., ваш бонус составит " + bonusMile + " бонусных миль.");
+    }
+}
